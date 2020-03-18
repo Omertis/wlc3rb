89dc887e73b83e16c49cf014c212e5e9a8580252
Omertis <36013948+Omertis@users.noreply.github.com>
2020-03-18 10:46:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ const fs = require('fs');
 if (member.user.bot) return;
 var Canvas = require('canvas')
 var jimp = require('jimp')
-const w = ['./img/image0 (14).jpg'];
+const w = ['./img/bull1.png'];
         let Image = Canvas.Image,
             canvas = new Canvas(749, 198),
             ctx = canvas.getContext('2d');
